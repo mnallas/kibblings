9b6b5bfc2735222dd76d91c6e5b7e7f0d4b7f9be
Napoleon Vuong <napolo101@gmail.com>
2020-08-25 19:47:23 -0700

    Added components for nav and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import './App.css';
+import NavBar from './components/NavBar'
+import Btn from './components/Button'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      {/* JumboTron Section??? */}
+      <Btn text="Submit"
+        variant="primary">
+      </Btn>
 
     </div>
   );
